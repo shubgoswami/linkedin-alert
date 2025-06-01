25c1fbfe7c7ef6f90ca770f33cad69606f500b91
Shubham <shubham.go19@fms.edu>
2025-06-01 21:49:40 +0530

    changed URL

--- job_scraper.py
+++ job_scraper.py
@@ -15,8 +15,8 @@ if not TELEGRAM_BOT_TOKEN or not TELEGRAM_USER_ID:
 
 # LinkedIn URLs to scrape jobs from
 LINKEDIN_URLS = [
-    "https://www.linkedin.com/jobs/search/?currentJobId=4241733995&f_TPR=r3600&geoId=102454443&keywords=product%20manager",
-    "https://www.linkedin.com/jobs/search/?currentJobId=4241733995&f_TPR=r3600&geoId=104305776&keywords=product%20manager"
+    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=102454443&keywords=product%20manager",
+    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=104305776&keywords=product%20manager"
 ]
 
 def fetch_rendered_html(url):
